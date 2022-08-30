f5a94118838ddcf9c61d14113c21beea789b16a5
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-08-30 19:18:09 +0900

    Update all-channel-delete.py

--- all-channel-delete.py
+++ all-channel-delete.py
@@ -3,7 +3,7 @@ from disnake.ext import commands
 
 token=""
 intents = disnake.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(intents=intents)
 
 @bot.slash_command(description="全チャンネルを削除")
 async def del_ch(ctx):
